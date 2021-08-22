77ea940a1caa049773e8db3a2ff025f563a22aeb
grjus <grjus@pm.me>
2021-08-22 21:19:59 +0200

    feat: Add mission endpoint with optional mission id

--- spacex-api/src/main/kotlin/com/grjus/spacex/api/SpaceXApi.kt
+++ spacex-api/src/main/kotlin/com/grjus/spacex/api/SpaceXApi.kt
@@ -2,14 +2,16 @@ package com.grjus.spacex.api
 
 
 import com.grjus.spacex.model.Launch
+import com.grjus.spacex.model.Mission
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 
 interface SpaceXApi {
     @GET("launches")
     fun fetchLaunches():Call<List<Launch>>
 
-    @GET("missions")
-    fun fetchMissions():Call<List<Launch>>
+    @GET("missions/{missionId}")
+    fun fetchMissions(@Path("missionId") missionId:String?):Call<List<Mission>>
 }
