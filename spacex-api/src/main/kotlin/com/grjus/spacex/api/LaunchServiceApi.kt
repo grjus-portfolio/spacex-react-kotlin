7c6ac9d9b049a04928178b657ae284960dc7004c
grjus <grjus@pm.me>
2021-08-23 00:03:49 +0200

    refactor: Refactor of launch service

--- spacex-api/src/main/kotlin/com/grjus/spacex/api/LaunchServiceApi.kt
+++ spacex-api/src/main/kotlin/com/grjus/spacex/api/LaunchServiceApi.kt
@@ -6,12 +6,15 @@ import com.grjus.spacex.model.Mission
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import java.util.*
 
 
-interface SpaceXApi {
+interface LaunchServiceApi {
     @GET("launches")
     fun fetchLaunches():Call<List<Launch>>
 
-    @GET("missions/{missionId}")
-    fun fetchMissions(@Path("missionId") missionId:String?):Call<List<Mission>>
+    @GET("launches/{flightNumber}")
+    fun fetchLaunch(@Path("flightNumber") flightNumber:Int):Call<Launch>
+
+
 }
