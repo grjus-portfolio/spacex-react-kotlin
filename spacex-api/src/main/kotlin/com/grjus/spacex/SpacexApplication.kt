f82ba4d6dd58910578b0fb84e7e88e460d22767e
grjus <grjus@pm.me>
2021-08-22 21:11:50 +0200

    feat: Add api main class

---
+++ spacex-api/src/main/kotlin/com/grjus/spacex/SpacexApplication.kt
@@ -0,0 +1,11 @@
+package com.grjus.spacex
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpacexApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpacexApplication>(*args)
+}
