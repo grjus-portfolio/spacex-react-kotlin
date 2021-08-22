fad5b0ec7e0f2bb47a2935b42d0cf26fd286eae0
grjus <grjus@pm.me>
2021-08-22 21:12:11 +0200

    test: Add api main function test

---
+++ spacex-api/src/test/kotlin/com/grjus/spacex/SpacexApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.grjus.spacex
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpacexApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
