57fce85645d141ff86d09c0fde9cd6c065dc01ce
grjus <grjus@pm.me>
2021-08-23 00:04:44 +0200

    feat: Add mission retrofit endpoints

---
+++ spacex-api/src/main/kotlin/com/grjus/spacex/api/MissionServiceApi.kt
@@ -0,0 +1,15 @@
+package com.grjus.spacex.api
+
+import com.grjus.spacex.model.Mission
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface MissionServiceApi {
+
+    @GET("missions")
+    fun fetchMissions():Call<List<Mission>>
+
+    @GET("missions/{missionId}")
+    fun fetchMission(@Path("missionId") missionId:String):Call<Mission>
+}
