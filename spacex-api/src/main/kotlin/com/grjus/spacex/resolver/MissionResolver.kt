6370fbe8ede4dc63ebbfcdb98aefff973680eb9c
grjus <grjus@pm.me>
2021-08-23 00:04:22 +0200

    feat: Add mission resolver

---
+++ spacex-api/src/main/kotlin/com/grjus/spacex/resolver/MissionResolver.kt
@@ -0,0 +1,35 @@
+package com.grjus.spacex.resolver
+
+import com.grjus.spacex.api.ApiCaller
+import com.grjus.spacex.model.Mission
+import com.netflix.graphql.dgs.DgsComponent
+import com.netflix.graphql.dgs.DgsData
+import com.netflix.graphql.dgs.InputArgument
+import kotlinx.coroutines.runBlocking
+import retrofit2.await
+
+@DgsComponent
+class MissionResolver {
+
+    suspend fun fetchMission(missionId: String):Mission{
+            return ApiCaller().missionApi.fetchMission(missionId).await()
+
+    }
+
+    suspend fun fetchMissions():List<Mission>{
+            return ApiCaller().missionApi.fetchMissions().await()
+
+    }
+
+    @DgsData(parentType = "Query", field = "missions")
+    fun resolveMissions() = runBlocking {
+        fetchMissions()
+    }
+
+    @DgsData(parentType = "Query", field = "mission")
+    fun resolveMission(@InputArgument("missionId") missionId: String) = runBlocking {
+        fetchMission(missionId)
+    }
+}
+
+
