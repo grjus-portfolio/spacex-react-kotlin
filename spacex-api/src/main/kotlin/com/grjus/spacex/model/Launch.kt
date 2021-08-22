cdb0d17dfe0476469879c4256f20eb13e6ad45dd
grjus <grjus@pm.me>
2021-08-22 21:06:44 +0200

    feat: Add launch data class

---
+++ spacex-api/src/main/kotlin/com/grjus/spacex/model/Launch.kt
@@ -0,0 +1,10 @@
+package com.grjus.spacex.model
+
+data class Launch(
+    val flight_number:Int,
+    val mission_name:String,
+    val launch_year:String,
+    val details:String,
+    val mission_id:String,
+
+)
